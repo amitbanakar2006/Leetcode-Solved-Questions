b03f68d29002393ad287f131df1205c6b61b052d
amitbanakar2006 <amitbanakar2006@gmail.com>
2026-05-07 12:08:45 +0530

    Time: 0 ms (100%), Space: 47 MB (46.56%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,29 +13,53 @@
  *     }
  * }
  */
-class Solution {
+class Solution {int maxx=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null){
-            return 0;
+        // if(root==null){
+        //     return 0;
+        // }
+    
+       height(root);
+          return  maxx;
+
         }
-        int l=height(root.left);
-        int r=height(root.right);
 
-        int onlyLeft=diameterOfBinaryTree( root.left);
-        int onlyright=diameterOfBinaryTree( root.right);
-
-        
-        
-        return Math.max( (l+r),Math.max(onlyLeft,onlyright));}
-        private int height(TreeNode node){
-            if(node==null){
+        private int height(TreeNode root){
+            if(root==null){
                 return 0;
             }
-            int a=height(node.left);
-            int b=height(node.right);
+            
+            int a=height(root.left);
+            int b= height(root.right);
+            maxx=Math.max(maxx,a+b);
+            return Math.max(a,b)+1;
+        }
 
-            return (Math.max(a,b))+1;
+
+
+
+
+        // if(root==null){
+        //     return 0;
+        // }
+        // int l=height(root.left);
+        // int r=height(root.right);
+
+        // int onlyLeft=diameterOfBinaryTree( root.left);//when tree like only left side so long..
+        // int onlyright=diameterOfBinaryTree( root.right);
+
+        
+        
+        // return Math.max( (l+r),Math.max(onlyLeft,onlyright));}
+        // private int height(TreeNode node){
+        //     if(node==null){
+        //         return 0;
+        //     }
+        //     int a=height(node.left);
+        //     int b=height(node.right);
+
+        //     return (Math.max(a,b))+1;
         
 
-    }
+    
 }
